6a3c643e978fffd10f9c1419a3e4511988d4ae67
Lorenzo Manacorda <lorenzo.manacorda@gmail.com>
2015-04-13 17:48:15 +0200

    play store decorator

---
+++ play_store_info.rb
@@ -0,0 +1,12 @@
+class PlayStoreInfo
+  include HTTParty
+  base_uri 'http://api.playstoreapi.com/v1.1/apps'
+
+  def self.fetch(fields, token, country_code)
+    if fields[:gpUniversalId]
+      get("/#{id}", query: { hl: country_code, key: token })
+    else
+      {}
+    end
+  end
+end
