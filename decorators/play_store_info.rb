46a7a0cbf871e66b66db3d06c538aeadbede357d
Lorenzo Manacorda <lorenzo.manacorda@gmail.com>
2015-04-14 14:30:40 +0200

    fix missing variable

--- decorators/play_store_info.rb
+++ decorators/play_store_info.rb
@@ -3,7 +3,7 @@ class PlayStoreInfo
   base_uri 'http://api.playstoreapi.com/v1.1/apps'
 
   def self.fetch(fields, token, country_code)
-    if fields[:gpUniversalId]
+    if id = fields[:gpUniversalId]
       get("/#{id}", query: { hl: country_code, key: token })
     else
       {}
