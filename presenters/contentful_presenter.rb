c1cf16dcc6397f9104ee43acaa7ed074a1783500
Lorenzo Manacorda <lorenzo.manacorda@gmail.com>
2015-04-16 15:32:54 +0200

    add image URLs to presenter

--- presenters/contentful_presenter.rb
+++ presenters/contentful_presenter.rb
@@ -1,10 +1,32 @@
 class ContentfulPresenter
   def self.build(fields)
     contentful_fields = fields[:contentful_fields]
-    itunes_fields     = fields[:itunes_fields]["universal"]
+    itunes_fields = fields[:itunes_fields]
+    image_urls = extract_image_urls(itunes_fields)
 
     contentful_fields.merge({
-      longDescription: itunes_fields["description"]
+      longDescription: itunes_fields["description"],
+      galleryTablet:   image_urls[:tablet],
+      galleryPhone:    image_urls[:phone]
     })
+  end
+
+  private
+  def self.extract_image_urls(fields)
+    phone = tablet = {}
+
+    if (fields["universal"])
+      phone = fields["universal"]["screenshotUrls"]
+      tablet = fields["universal"]["ipadScreenshotUrls"]
+    else
+      if (fields["tablet"])
+        tablet = fields["tablet"]["ipadScreenshotUrls"]
+      end
+      if (fields["phone"])
+        phone = fields["phone"]["screenshotUrls"]
+      end
+    end
+
+    { phone: phone, tablet: tablet }
   end
 end
