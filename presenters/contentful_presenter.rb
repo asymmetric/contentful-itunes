98484c09308613260da7588fb1d305ef42478edf
Lorenzo Manacorda <lorenzo.manacorda@gmail.com>
2015-04-17 17:15:24 +0200

    only parse itunes fields if they're present

--- presenters/contentful_presenter.rb
+++ presenters/contentful_presenter.rb
@@ -2,13 +2,20 @@ class ContentfulPresenter
   def self.build(fields)
     contentful_fields = fields[:contentful_fields]
     itunes_fields = fields[:itunes_fields]
-    image_urls = extract_image_urls(itunes_fields)
 
-    contentful_fields.merge({
-      longDescription: itunes_fields["description"],
-      galleryTablet:   image_urls[:tablet],
-      galleryPhone:    image_urls[:phone]
-    })
+    if !itunes_fields.empty?
+      image_urls = extract_image_urls(itunes_fields)
+      description = extract_description(itunes_fields)
+
+      contentful_fields.merge({
+        longDescription: description,
+        galleryTablet:   image_urls[:tablet],
+        galleryPhone:    image_urls[:phone]
+      })
+    else
+      contentful_fields
+    end
+
   end
 
   private
