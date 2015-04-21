2832ac32a8a1f9d9effe9e022f97c1f9278eb8ad
Lorenzo Manacorda <lorenzo.manacorda@gmail.com>
2015-04-21 14:27:43 +0200

    add client block in sidekiq config

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -6,3 +6,7 @@ require_relative '../app_list'
 Sidekiq.configure_server do |config|
   config.redis = { namespace: 'contentful' }
 end
+
+Sidekiq.configure_client do |config|
+  config.redis = { namespace: 'contentful', size: 1 }
+end
