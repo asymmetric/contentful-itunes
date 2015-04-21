5b80642505b035a7b17a634192c99050c8d0f6a9
Lorenzo Manacorda <lorenzo.manacorda@gmail.com>
2015-04-21 13:47:13 +0200

    add all locales

--- sidekiq/sync_worker.rb
+++ sidekiq/sync_worker.rb
@@ -6,11 +6,23 @@ class SyncWorker
   SETTINGS = {
     access_token: "access-token",
     space: "space-id",
-    apps_type: 'content-type-id'
+    apps_type: "content-type-id"
   }
 
+  LOCALES = %w/
+    de-DE
+    de-AT
+    de-CH
+    se-SE
+    no-NO
+    dk-DK
+    pl-PL
+    nl-NL
+    nl-BE
+  /
+
   def perform
-    %w/de-DE/.each do |locale|
+    LOCALES.each do |locale|
       puts "synching locale #{locale}"
       app_list = AppList.new SETTINGS.merge locale: locale
       app_list.sync
