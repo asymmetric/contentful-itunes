73654174531d5c52c7504aace1b35827dd8e6664
Lorenzo Manacorda <lorenzo.manacorda@gmail.com>
2015-05-27 16:39:59 +0200

    fix env variable name

--- config/airbrake.rb
+++ config/airbrake.rb
@@ -1,4 +1,4 @@
 Airbrake.configure do |config|
   config.api_key = ENV['AIRBRAKE_API_KEY']
-  config.environment_name = ENV['RACK_ENV'] || "production"
+  config.environment_name = ENV['APP_ENV'] || "production"
 end
